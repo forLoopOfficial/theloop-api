33d7ef78f4e2af8083f27b6e125be7917a08bc60
Timi Ajiboye <timi@helloworld.ng>
2017-02-18 01:34:10 +0100

    chore: set counter cache columns to default 0

--- db/migrate/20170217220407_add_count_attributes_to_respective_models.rb
+++ db/migrate/20170217220407_add_count_attributes_to_respective_models.rb
@@ -1,7 +1,7 @@
 class AddCountAttributesToRespectiveModels < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :likes_count, :integer
-    add_column :users, :posts_count, :integer
-    add_column :posts, :likes_count, :integer
+    add_column :users, :likes_count, :integer, default: 0
+    add_column :users, :posts_count, :integer, default: 0
+    add_column :posts, :likes_count, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217220407) do
+ActiveRecord::Schema.define(version: 20170217214146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,8 @@ ActiveRecord::Schema.define(version: 20170217220407) do
   create_table "posts", force: :cascade do |t|
     t.string   "body"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "likes_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,8 +47,6 @@ ActiveRecord::Schema.define(version: 20170217220407) do
     t.text     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
-    t.integer  "likes_count"
-    t.integer  "posts_count"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
