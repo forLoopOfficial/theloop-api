7a781cc62d22331e2e037a7827216b93d3d503e9
Timi Ajiboye <timi@helloworld.ng>
2017-02-18 01:26:26 +0100

    chore: expose headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'total', 'perPage', 'page', 'If-Modified-Since']
+
   end
 end
